4b6ad21053e70570b956e3621d5aaade35a1c45a
Jakub Frelik <git@jfrelik.dev>
2024-04-04 18:48:42 +0200

    refactor: UserService overhaul

--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -1,69 +1,56 @@
-﻿using System;
+﻿using LegacyApp.Interfaces;
+using System;
 
 namespace LegacyApp
 {
     public class UserService
     {
-        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        private readonly IClientRepository _clientRepository;
+        private readonly IUserCreditService _userCreditService;
+        private readonly UserValidator _validator;
+        private readonly UserFactory _userFactory;
+
+
+        [Obsolete("Will be removed in the 0.2")]
+        public UserService()
         {
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
-            {
-                return false;
-            }
+            _clientRepository = new ClientRepository();
+            _userCreditService = new UserCreditService();
+            _validator = new UserValidator();
+            _userFactory = new UserFactory(_userCreditService);
+        }
 
-            if (!email.Contains("@") && !email.Contains("."))
-            {
-                return false;
-            }
+        public UserService(IClientRepository clientRepository, IUserCreditService userCreditService)
+        {
+            _clientRepository = clientRepository;
+            _userCreditService = userCreditService;
+        }
 
+        private int CalculateUserAge(DateTime dateOfBirth)
+        {
             var now = DateTime.Now;
             int age = now.Year - dateOfBirth.Year;
+            // Go back to the year the person was born in case of a leap year
             if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+            return age;
+        }
+        
+        private bool IsUserBelowCreditLimit(User user)
+        {
+            return user.HasCreditLimit && user.CreditLimit <= 500;
+        }
 
-            if (age < 21)
-            {
-                return false;
-            }
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (!_validator.ValidateName(firstName) || !_validator.ValidateName(lastName) || !_validator.ValidateEmail(email)) return false;
+            
+            int age = CalculateUserAge(dateOfBirth);
+            if (age < 21) return false;
 
-            var clientRepository = new ClientRepository();
-            var client = clientRepository.GetById(clientId);
-
-            var user = new User
-            {
-                Client = client,
-                DateOfBirth = dateOfBirth,
-                EmailAddress = email,
-                FirstName = firstName,
-                LastName = lastName
-            };
-
-            if (client.Type == "VeryImportantClient")
-            {
-                user.HasCreditLimit = false;
-            }
-            else if (client.Type == "ImportantClient")
-            {
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
-            else
-            {
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
-            }
-
-            if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
-                return false;
-            }
+            var client = _clientRepository.GetById(clientId);
+            var user = _userFactory.CreateUser(firstName, lastName, email, dateOfBirth, client);
+            
+            if (IsUserBelowCreditLimit(user)) return false;
 
             UserDataAccess.AddUser(user);
             return true;
